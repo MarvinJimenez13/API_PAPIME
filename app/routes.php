8eaeff30983d38f61f275d052924ae72771fc793
Roberto <marvin.jimenez@sferea.com>
2022-06-28 01:33:44 -0500

    test eliminarProfesor options put

--- app/routes.php
+++ app/routes.php
@@ -33,7 +33,7 @@ return function (App $app) {
         return $response->withStatus($data['response_code']);
     });
 
-    $app->post('/admin/eliminarProfesor', function(Request $request, Response $response){
+    $app->put('/admin/eliminarProfesor', function(Request $request, Response $response){
         $data = Admin::eliminarProfesor(json_encode($request->getParsedBody()));
         $response->getBody()->write(json_encode($data));
 
